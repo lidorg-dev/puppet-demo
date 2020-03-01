bf807eef07ad2c7d1e5dc0cae6dcb5344d2e6602
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2020-03-01 22:02:39 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -13,5 +13,19 @@ class minecraft {
    package {'java-1.8.0-openjdk':
      ensure => present,
      }
+     
+   file {'/opt/minecraft/eula.txt':
+     ensure => file,
+     content => 'eula=true'
    }  
    
+   file {'/etc/systemd/system/minecraft.service':
+      ensure => file,
+      source => 'puppet:///modules/minecraft/minecraft.service',
+      }
+      
+   service {'minecraft':
+     ensure => running,
+     enable => true,
+     }
+}
