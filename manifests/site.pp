033169a6e4a98f4a967e7b0028cd1e9c880cddc8
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2020-03-01 21:26:49 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node 'puppet-server' {
    include role::master_server
  }
-node 'node-1' {
+node 'node-2' {
    include role::app_server
  } 
 
 
-node 'node-2' {
+node 'node-1' {
 
     include role::db_server
 }
